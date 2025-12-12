c7e10876ee4d66d2f40808dfca3abd9c35fdcac7
DerGreed <joel.hilberg@gmail.com>
2025-12-12 15:27:10 +0100

    added env var checking

--- scraper.py
+++ scraper.py
@@ -3,11 +3,18 @@ import requests
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-load_dotenv()
+load_dotenv('./.env')
 
-client = MongoClient(os.getenv("MONGO_URI"))
-db = client[os.getenv("MONGO_DB")]
-collection = db[os.getenv("MONGO_COLLECTION")]
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB = os.getenv("MONGO_DB")
+MONGO_COLLECTION = os.getenv("MONGO_COLLECTION")
+
+if not (MONGO_URI and MONGO_DB and MONGO_COLLECTION):
+    raise RuntimeError("Required env vars missing: MONGO_URI, MONGO_DB, MONGO_COLLECTION")
+
+client = MongoClient(MONGO_URI)
+db = client[MONGO_DB]
+collection = db[MONGO_COLLECTION]
 
 def get_latest_comic_number():
     resp = requests.get("https://xkcd.com/info.0.json")
@@ -17,10 +24,10 @@ def get_latest_comic_number():
 def fetch_comic(num):
     url = f"https://xkcd.com/{num}/info.0.json"
     resp = requests.get(url)
-    
+
     if resp.status_code != 200:
         return None
-    
+
     return resp.json()
 
 def save_comic(doc):
